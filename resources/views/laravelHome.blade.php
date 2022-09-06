3c0b189b1d0c9855f77e294b4bb5a84db9359ff5
Bice93 <bianca.cerulo@hotmail.it>
2022-09-06 15:41:48 +0200

    Create new file laravelHome

--- resources/views/laravelHome.blade.php
+++ resources/views/laravelHome.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel primi passi</title>
+    <title>Laravel</title>
 </head>
 <body>
-    <h1>Hello World</h1>
-    <h2>Laravel</h2>
-    <a href="/pageLaravelHome">Cos'è laravel</a>
+    <h1>Laravel</h1>
+    <p>Laravel è un framework open source di tipo MVC scritto in PHP per lo sviluppo di applicazioni web,
+        creato nel 2011 da Taylor Otwell come derivazione di Symfony.</p>
 </body>
 </html>
